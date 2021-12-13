cd70e2d1b1cfaae443503261a97296cd963cead0
Generalsoi <ikhinobelesuccess@gmail.com>
2021-12-13 08:02:54 +0100

    updates to the kurtis app

--- src/components/sectioinFiveGrid.jsx
+++ src/components/sectioinFiveGrid.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const SectionFiveGrid = () => {
-  return <div>Grid</div>;
+const SectionFiveGrid = (props) => {
+  const { gridHeader, gridContent, gridImage, gridStyle } = props;
+  return (
+    <div className={gridStyle}>
+      <div>
+        <h5>{gridHeader}</h5>
+        <p>{gridContent}</p>
+      </div>
+      <img src={gridImage} alt="grid-img" />
+    </div>
+  );
 };
 
 export default SectionFiveGrid;
--- src/components/sectionFive.jsx
+++ src/components/sectionFive.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import "../css/sectionFive.css";
 import SectionFiveGrid from "./sectioinFiveGrid";
+import MiroodlesOne from "../images/MiroodlesColorComposition.png";
+import MiroodlesTwo from "../images/MiroodlesColorCompositionTwo.png";
+import MiroodlesThree from "../images/MiroodlesColorCompositionThree.png";
 
 const SectionFive = () => {
   return (
     <div className="section-five">
-      <p>
-        Still more? <a href="">Take a look to our features games</a>
-      </p>
-
-      <h2 className="sectionFiveH2">How does Kurtis work?</h2>
-      <SectionFiveGrid />
+      <h3>How does Kurtis work?</h3>
+      <SectionFiveGrid
+        gridStyle="grid-one"
+        gridImage={MiroodlesOne}
+        gridHeader="Create"
+        gridContent="It only takes minutes to create a learning game or trivia quiz on any topic, in any language."
+      />
+      <SectionFiveGrid
+        gridStyle="grid-two"
+        gridImage={MiroodlesTwo}
+        gridHeader="Host or Share"
+        gridContent="Host a live game with questions on a big screen or share a game with remote players."
+      />
+      <SectionFiveGrid
+        gridStyle="grid-one"
+        gridImage={MiroodlesThree}
+        gridHeader="Play"
+        gridContent="Game on! Join a kahoot with a PIN provided by the host and answer questions on your device."
+      />
     </div>
   );
 };
